a96faaf5ab99bdf2e6bc78d016e41fdd4299de50
Szymon Walaszek <kontakt@szymonwalaszek.pl>
2022-03-08 06:32:06 +0100

    Start project and add some basic functions

---
+++ src/ATM.java
@@ -0,0 +1,30 @@
+public class ATM implements Functionality {
+
+    private int balance;
+    private String PIN;
+
+    @Override
+    public void menu() {
+
+    }
+
+    @Override
+    public void run() {
+        
+    }
+
+    @Override
+    public void checkBalance() {
+
+    }
+
+    @Override
+    public void withdraw() {
+
+    }
+
+    @Override
+    public void deposit() {
+
+    }
+}
---
+++ src/Functionality.java
@@ -0,0 +1,8 @@
+public interface Functionality {
+
+    void run();
+    void checkBalance();
+    void withdraw();
+    void deposit();
+    void menu();
+}
